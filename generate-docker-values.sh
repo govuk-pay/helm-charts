94f7d9239e994f79167f5f69e60f1c03644144c3
Joe Roberts <joe.roberts@digital.cabinet-office.gov.uk>
2026-03-18 16:03:43 +0000

    Add imagepullsecret

--- generate-docker-values.sh
+++ generate-docker-values.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
-cat <<EOF > values/pay-app/values.docker.yaml
-dockerPassword: $(aws ecr get-login-password)
-EOF
+AWS_ACCOUNT=223851549868
+AWS_REGION=eu-west-1
+kubectl create secret docker-registry awsecr \
+  --namespace $NAMESPACE \
+  --docker-server=${AWS_ACCOUNT}.dkr.ecr.${AWS_REGION}.amazonaws.com \
+  --docker-username=AWS \
+  --docker-password=$(aws ecr get-login-password)
